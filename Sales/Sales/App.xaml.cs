2ab7f790bdbfb8ee4f6b443d3f2d98dee5d3da16
Juan Ocampo <david.ocampo.3928@gmail.com>
2018-09-22 13:48:44 -0500

    Login with Facebook

--- Sales/Sales/App.xaml.cs
+++ Sales/Sales/App.xaml.cs
@@ -10,6 +10,9 @@ namespace Sales
     using Helpers;
     using Newtonsoft.Json;
     using Sales.Common.Models;
+    using System;
+    using System.Threading.Tasks;
+    using Sales.Services;
 
     public partial class App : Application
     {
@@ -43,6 +46,43 @@ namespace Sales
         }
         #endregion
 
+        #region Methods
+        public static Action HideLoginView
+        {
+            get
+            {
+                return new Action(() => Current.MainPage = new NavigationPage(new LoginPage()));
+            }
+        }
+
+        public static async Task NavigateToProfile(TokenResponse token)
+        {
+            if (token == null)
+            {
+                Application.Current.MainPage = new NavigationPage(new LoginPage());
+                return;
+            }
+
+            Settings.IsRemembered = true;
+            Settings.AccessToken = token.AccessToken;
+            Settings.TokenType = token.TokenType;
+
+            var apiService = new ApiService();
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var prefix = Application.Current.Resources["UrlPrefix"].ToString();
+            var controller = Application.Current.Resources["UrlUsersController"].ToString();
+            var response = await apiService.GetUser(url, prefix, $"{controller}/GetUser", token.UserName, token.TokenType, token.AccessToken);
+            if (response.IsSuccess)
+            {
+                var userASP = (UserASP)response.Result;
+                MainViewModel.GetInstance().UserASP = userASP;
+                Settings.UserASP = JsonConvert.SerializeObject(userASP);
+            }
+
+            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            Application.Current.MainPage = new MasterPage();
+        }
+
         protected override void OnStart()
         {
             // Handle when your app starts
@@ -57,5 +97,6 @@ namespace Sales
         {
             // Handle when your app resumes
         }
+        #endregion
     }
 }
